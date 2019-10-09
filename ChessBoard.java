33e14743c9bcb9bc27224445e931a4c80289c59e
ppnnx <52230546+ppnnx@users.noreply.github.com>
2019-10-09 09:15:41 +0700

    Update ChessBoard.java

--- ChessBoard.java
+++ ChessBoard.java
@@ -8,17 +8,58 @@ public class ChessBoard {
 	// constructor
 	public ChessBoard(){
 		// place 32 pieces on board
-		addPiece(new King(0, 3, 0));   // row col color 0 = black
-		addPiece(new Queen(0, 4, 0));
+		addPiece(new Rook	(0, 0, 0));
+		addPiece(new Knight	(0, 1, 0));
+		addPiece(new Bishop	(0, 2, 0));
+		addPiece(new King	(0, 3, 0));   // row col color 0 = black
+		addPiece(new Queen	(0, 4, 0));
+		addPiece(new Bishop	(0, 5, 0));
+		addPiece(new Knight	(0, 6, 0));
+		addPiece(new Rook	(0, 7, 0));
+		addPiece(new Pawn	(1, 0, 0));
+		addPiece(new Pawn	(1, 1, 0));
+		addPiece(new Pawn	(1, 2, 0));
+		addPiece(new Pawn	(1, 3, 0));
+		addPiece(new Pawn	(1, 4, 0));
+		addPiece(new Pawn	(1, 5, 0));
+		addPiece(new Pawn	(1, 6, 0));
+		addPiece(new Pawn	(1, 7, 0));
 		 
-		addPiece(new King(7, 3, 1));   // row col color 1 = white
-		addPiece(new Queen(7, 4, 1));
+		addPiece(new Rook	(7, 0, 1));
+		addPiece(new Knight	(7, 1, 1));
+		addPiece(new Bishop	(7, 2, 1));
+		addPiece(new King	(7, 3, 1));   // row col color 1 = white
+		addPiece(new Queen	(7, 4, 1));
+		addPiece(new Bishop	(7, 5, 1));
+		addPiece(new Knight	(7, 6, 1));
+		addPiece(new Rook	(7, 7, 1));
+		addPiece(new Pawn	(6, 0, 1));
+		addPiece(new Pawn	(6, 1, 1));
+		addPiece(new Pawn	(6, 2, 1));
+		addPiece(new Pawn	(6, 3, 1));
+		addPiece(new Pawn	(6, 4, 1));
+		addPiece(new Pawn	(6, 5, 1));
+		addPiece(new Pawn	(6, 6, 1));
+		addPiece(new Pawn	(6, 7, 1));
 	}
 	
 	// board setter
 	void addPiece(ChessPiece piece){
 		
 		board[piece.row][piece.col] = piece;
+	}
+	void removePiece(int row,int col) {
+		board[row][col] = null;
+	}
+	
+	void movePiece(Coordinate from,Coordinate to) {
+		ChessPiece cur = board[from.row][from.col];
+		if(cur.isValidMove(this, to.row, to.col)) {
+			cur.col = to.col;
+			cur.row = to.row;
+			board[from.row][from.col] = null;
+			board[to.row][to.col] = cur;	
+		}
 	}
 	
 	// board getter
