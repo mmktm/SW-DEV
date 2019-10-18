3d1ad50097369a3abbe8c6762d1d409fd5cb3111
ppnnx <52230546+ppnnx@users.noreply.github.com>
2019-10-18 19:54:26 +0700

    Update Queen.java

--- Queen.java
+++ Queen.java
@@ -19,7 +19,22 @@ public class Queen extends ChessPiece {
 
 	@Override
 	boolean isValidMove(ChessBoard board, int row, int col) {
-		return true;
+		for(int i = 0; i < 8; i++) {
+			
+			if((this.row - i) == row && (this.col + i) == col)
+				return true;
+			if((this.col - i) == col && (this.row + i) == row)
+				return true;		
+			if((this.row + i) == row && (this.col + i) == col)
+				return true;
+			if((this.col - i) == col && (this.row - i) == row)
+				return true;
+			if(this.col == col)
+				return true;
+			if(this.row == row)
+				return true;
+		}	
+		System.out.println("Can't Move!");
+		return false;
 	}
-
 }
